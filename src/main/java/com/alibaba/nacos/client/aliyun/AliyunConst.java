59b25ca4b0a611b4c4a22bd8594d8cb4cd5cedba
luyanbo <robert.lyb@alibaba-inc.com>
2021-04-21 11:44:44 +0800

    init for cipher

---
+++ src/main/java/com/alibaba/nacos/client/aliyun/AliyunConst.java
@@ -0,0 +1,10 @@
+package com.alibaba.nacos.client.aliyun;
+
+/**
+ * the Const Values of Aliyun.
+ *
+ * @author luyanbo(RobberPhex)
+ */
+public class AliyunConst {
+    public static final String KMS_ENDPOINT = "kmsEndpoint";
+}
